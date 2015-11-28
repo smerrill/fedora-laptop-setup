8d6d7f3fac2aa4c7af6e93034a1d8944d0a91ee9
Steven Merrill <steven.merrill@gmail.com>
2015-11-28 17:49:58 -0500

    Add account parameter, apply script.

--- apply.sh
+++ apply.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-ansible-playbook -i hosts -K playbook.yml
+if ! type -p ansible-playbook >/dev/null; then
+  echo "Please install Ansible to continue."
+  exit 0
+fi
+
+if [ -z $1 ]; then
+  ACCOUNT=$USER
+else
+  ACCOUNT=$1
+fi
+
+echo "Setting up the computer for ${ACCOUNT}. You can change this by passing a parameter to this script."
+ansible-playbook -e account=${ACCOUNT} -i hosts -K playbook.yml
